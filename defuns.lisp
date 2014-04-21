638636a8923de1106bd4e7fd357c4d4de556ed79
Alex <aaermolov@gmail.com>
2014-04-21 22:19:50 +0400

    merged defuns

--- defuns.lisp
+++ defuns.lisp
@@ -7,6 +7,34 @@
 ;;;
 ;;;
 
+(defun defkey-top (key cmd)
+  (define-key *top-map* (kbd key) cmd))
+
+(defun defkey-root (key cmd)
+  (define-key *root-map* (kbd key) cmd))
+
+(defun defkey-in-map (keymap key cmd)
+  (define-key keymap (kbd key) cmd))
+
+(defmacro defkeys-top (&rest keys)
+  (let ((ks (mapcar #'(lambda (k) (cons 'defkey-top k)) keys)))
+    `(progn ,@ks)))
+
+(defmacro defkeys-root (&rest keys)
+  (let ((ks (mapcar #'(lambda (k) (cons 'defkey-root k)) keys)))
+    `(progn ,@ks)))
+
+(defmacro build-keymap (&rest keys)
+  `(let ((m (make-sparse-keymap)))
+     ,@(mapcar #'(lambda (k) `(defkey-in-map m ,(first k) ,(second k))) keys)
+     m))
+
+(defmacro populate-keymap (keymap &rest keys)
+  (setf keymap
+  `(let ((m (make-sparse-keymap)))
+     ,@(mapcar #'(lambda (k) `(defkey-in-map m ,(first k) ,(second k))) keys)
+     m)))
+
 (defun current-window-title ()
   (let ((current-window (current-window)))
     (if current-window
