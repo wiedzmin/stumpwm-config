109fd2838e08f7053c49d9e42cf4a23c26f4a417
Alex <aaermolov@gmail.com>
2014-04-28 00:00:15 +0400

    variables: reorganized for readability

--- variables.lisp
+++ variables.lisp
@@ -7,19 +7,22 @@
 ;;;
 ;;;
 
-(setf *normal-border-width* 0
-      *maxsize-border-width* 0
-      *transient-border-width* 1
-      *window-border-style* :thick)     ; :thick :thin :tight :none
-(setf *mode-line-position* :bottom)
-(setf *frame-number-map* "1234567890")
-(setf *startup-message* "Never Stop Hacking!")
-(setf *run-or-raise-all-screens* t)
-(setf *mode-line-foreground-color* "DarkSeaGreen")
-(setf *mode-line-timeout* 10)
+(defparameter *simple-resize-increment* 100)
+
+;; borders
+(setf *maxsize-border-width* 0)
+(setf *transient-border-width* 1)
 (setf *normal-border-width* 1)
-(setf *disk-modeline-fmt* "%m: %a/%s")
-(setf *time-modeline-string* "%d-%m-%Y ^3*^B%H:%M^b^n %a")
+(setf *window-border-style* :thin)
+
+(set-focus-color "red")
+(set-win-bg-color "black")
+(set-border-color "grey16")
+(set-msg-border-width 0)
+
+
+;; modeline
+(setf *mode-line-position* :bottom)
 (setf *screen-mode-line-format*
      (list " "
            "^B[^b"
@@ -31,31 +34,24 @@
            `(:eval (mode-line-groups-list))
            "^7*(^[^n ^]" `(:eval (window-title-and-notifications-with-fix-length 200 20)) ")^n "
            ))
-(setf *window-format* "%n%s%t")
+(setf *mode-line-foreground-color* "DarkSeaGreen")
+(setf *mode-line-timeout* 10)
+(setf *disk-modeline-fmt* "%m: %a/%s")
+(setf *time-modeline-string* "%d-%m-%Y ^3*^B%H:%M^b^n %a")
 
-;; Window border colors.
-(set-focus-color "red")
-(set-win-bg-color "black")
+;; gravity
+(setf *input-window-gravity* :center)
+(setf *message-window-gravity* :bottom-right)
 
-(setf *window-border-style* :thin)      ; :thick :thin :tight :none
 (set-normal-gravity :bottom)
 (set-maxsize-gravity :center)
 (set-transient-gravity :center)
 
+
+;; various
+(setf *frame-number-map* "1234567890")
+(setf *startup-message* "Never Stop Hacking!")
+(setf *run-or-raise-all-screens* t)
+(setf *window-format* "%n%s%t")
 (set-fg-color "yellow")
-(set-border-color "grey16")
-(set-msg-border-width 0)
-(setf *input-window-gravity* :center)
-
-;; Set the message and input box to the bottom right. This way it overlaps with mode-line.
-(setf *message-window-gravity* :bottom-right)
-;; in seconds, how long a message will appear for. This must be an integer.
-(setf *timeout-wait* 5)
-
-(defparameter *simple-resize-increment* 100)
-
-;; (defparameter X-TERM "urxvt"
-;;   "What shall be the command run when we want an X terminal?")
-
-;; (defparameter X-WWW-BROWSER "firefox"
-;;   "What GUI WWW browser shall we use?")
+(setf *timeout-wait* 3)
