993d74cfc450675b983f3fd8cd350d7ff722d298
Alex <aaermolov@gmail.com>
2014-10-06 00:09:18 +0400

    removed now obsolete workaround for video playback freezes on external monitor
    ii was 3D acceleration causing freezes on external monitor, see
    https://wiki.archlinux.org/index.php/Intel_graphics#X_freeze.2Fcrash_with_intel_driver

--- groups.lisp
+++ groups.lisp
@@ -1,13 +1,5 @@
 (clear-window-placement-rules)
 
-;; by default force video players to go to frame 0
-;; otherwise they freeze up stumpwm being residing
-;; on external monitor occasionally (VGA-specific)
-(define-frame-preference "Default"
-  (0 t   t :class "Qmpdclient")
-  (0 t   t :class "Vlc")
-  (0 t   t :class "Smplayer"))
-
 ;; NOTE for more elaborate bunch of groups see xmonad config
 
 (defcommand rearrange-windows () ()
