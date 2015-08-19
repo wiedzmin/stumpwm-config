8c276963585e1139970feecf230d8fae59a04819
Alex Ermolov <aermolov@mirantis.com>
2015-08-19 18:50:57 +0300

    commands: fixed accidental mistake in defwebjump macro

--- commands.lisp
+++ commands.lisp
@@ -179,12 +179,13 @@ rules."
 
 (defmacro defwebjump (caption url &key (map *web-keymap*) (key nil))
   (let ((command-name (concat-as-symbol "custom/open-" (string-downcase (substitute #\- #\Space caption)))))
-    `(defcommand
-         ,command-name () ()
-       ,(format nil "Open ~a" caption)
-       (open-in-browser ,url))
-    (when key
-      `(define-key ,map (kbd ,key) ,(string-downcase command-name)))))
+    `(progn
+       (defcommand
+           ,command-name () ()
+         ,(format nil "Open ~a" caption)
+         (open-in-browser ,url))
+       (when ,key
+         (define-key ,map (kbd ,key) ,(string-downcase command-name))))))
 
 (defwebjump "Gmail" "https://mail.google.com/mail/u/0/#inbox" :key "m")
 (defwebjump "Github" "https://github.com/wiedzmin" :key "g")
