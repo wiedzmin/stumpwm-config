c78422404aef02a4ee870af9bff30df2d8ae6282
Alex Ermolov <aermolov@mirantis.com>
2015-11-02 00:20:29 +0300

    commands: +sakura -skype applications update

--- commands.lisp
+++ commands.lisp
@@ -232,7 +232,7 @@ rules."
 (define-application vlc :map *raise-keymap* :key "l" :pullp t :pull-map *pull-keymap* :pull-key "l")
 (define-application fbreader :map *raise-keymap* :key "2" :pullp t :pull-map *pull-keymap* :pull-key "2")
 (define-application zathura :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
-(define-application skype :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
+(define-application sakura :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
 (define-application wicd-gtk :command "wicd-gtk -n" :class "Wicd-client.py" :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
 (define-application pavucontrol :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
 (define-application krdc :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
