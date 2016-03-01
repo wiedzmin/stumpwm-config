a2345ce3f3b7ad26f2603e1f6c95e94fe5fdc46d
Alex Ermolov <aermolov@mirantis.com>
2016-03-01 12:14:27 +0300

    added Skype application back, but not keybinded

--- custom.lisp
+++ custom.lisp
@@ -299,6 +299,7 @@ rules."
 (define-application fbreader :map *raise-keymap* :key "2" :pullp t :pull-map *pull-keymap* :pull-key "2")
 (define-application zathura :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
 (define-application sakura :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
+(define-application skype :map *raise-keymap* :pullp t :pull-map *pull-keymap* :binded nil)
 (define-application wicd-gtk :command "wicd-gtk -n" :class "Wicd-client.py" :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
 (define-application pavucontrol :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
 (define-application krdc :map *raise-keymap* :pullp t :pull-map *pull-keymap*)
